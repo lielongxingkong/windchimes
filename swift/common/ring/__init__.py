3c55e0afc0176275d8e54635d5394794fa731f05
lielongxingkong <zzl_1164@126.com>
2013-11-17 20:38:56 +0800

    Add FingerRing to import

--- swift/common/ring/__init__.py
+++ swift/common/ring/__init__.py
@@ -1,8 +1,9 @@
-from swift.common.ring.ring import RingData, Ring
+from swift.common.ring.ring import RingData, Ring, FingerRing
 from swift.common.ring.builder import RingBuilder
 
 __all__ = [
     'RingData',
     'Ring',
+    'FingerRing',
     'RingBuilder',
 ]
